db1d20ffb5d33ffd70155fbf12607b38c8611447
Distance <egoistgray@gmail.com>
2024-05-23 16:53:44 +0800

    feat: implemented build script

---
+++ build.ps1
@@ -0,0 +1,17 @@
+$buildFolder = "$PSScriptRoot/build"
+if (!(Test-Path $buildFolder -PathType Container)) {
+    New-Item -ItemType Directory -Force -Path $buildFolder
+}
+
+cd $buildFolder
+cmake .. `
+   -GNinja `
+   -DCMAKE_C_COMPILER=clang-cl `
+   -DCMAKE_CXX_COMPILER=clang-cl `
+   -DLLAMA_NATIVE=OFF `
+   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 `
+   -DCMAKE_BUILD_TYPE=Release `
+   -DLLAMA_VULKAN=ON 
+ninja 
+ninja install 
+cd ..
