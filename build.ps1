2d7615cce1031647828c52d414e7c5c838d9e34c
Distance <egoistgray@gmail.com>
2024-06-01 03:47:40 +0800

    feat: working build (not working extension)

--- build.ps1
+++ build.ps1
@@ -2,9 +2,11 @@ cmake -B build `
    -G Ninja `
    -DCMAKE_C_COMPILER=clang-cl `
    -DCMAKE_CXX_COMPILER=clang-cl `
-   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON `
    -DCMAKE_BUILD_TYPE=Release `
-   -DLLAMA_NATIVE=OFF
+   -DLLAMA_NATIVE=OFF `
+   -DLLAMA_CUDA=ON
 
-cmake --build build
-cmake --install build
+cd build
+ninja -j24
+ninja install
+cd ..
