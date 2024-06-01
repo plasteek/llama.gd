bef6fbbe3bdfebcbb0bf8ac04a874abaf24b5f99
Distance <egoistgray@gmail.com>
2024-06-01 19:10:08 +0800

    feat: implemented vulkan build

--- build-vulkan.ps1
+++ build-vulkan.ps1
@@ -1,10 +1,9 @@
-Write-Output "CUDA is not compatible with GDExtension since the compile failed. Use Vulkan if this does not work"
 cmake -B build `
    -G Ninja `
    -D CMAKE_C_COMPILER=clang-cl `
    -D CMAKE_CXX_COMPILER=clang-cl `
    -D CMAKE_BUILD_TYPE=Release `
-   -D LLAMA_CUDA=ON `
+   -D LLAMA_VULKAN=ON `
    -D CMAKE_EXPORT_COMPILE_COMMANDS=ON `
    -D LLAMA_NATIVE=OFF
 cmake --build build -j24
