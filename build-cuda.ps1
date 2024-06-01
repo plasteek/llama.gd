53bee949033ab06c6d92769b5f9e8f627644557c
Distance <egoistgray@gmail.com>
2024-06-01 18:39:20 +0800

    feat: separated build for cuda and cpu

--- build-cuda.ps1
+++ build-cuda.ps1
@@ -3,6 +3,7 @@ cmake -B build `
    -D CMAKE_C_COMPILER=clang-cl `
    -D CMAKE_CXX_COMPILER=clang-cl `
    -D CMAKE_BUILD_TYPE=Release `
+   -D LLAMA_CUDA=ON `
    -D CMAKE_EXPORT_COMPILE_COMMANDS=ON `
    -D LLAMA_NATIVE=OFF
 cmake --build build -j24
