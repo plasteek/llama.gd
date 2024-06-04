c3071d130ceee54426bb5bd4ec75fc5bb7bc367f
Distance <egoistgray@gmail.com>
2024-06-04 18:05:13 +0800

    fix: header default not removed when changes were made to batch decoding

--- src/llama_utils.hpp
+++ src/llama_utils.hpp
@@ -4,7 +4,7 @@
 #include <llama.h>
 #include <common.h>
 
-void batch_decode_tokens(int batch_size, llama_context *ctx, std::vector<llama_token> tokens, int start_index = 0, bool sample_last_logit = true);
+void batch_decode_tokens(int batch_size, llama_context *ctx, std::vector<llama_token> tokens, int start_index = 0);
 void insert_without_bos(std::vector<llama_token> *embd, std::vector<llama_token> *tokens, llama_token bos);
 std::vector<llama_token> construct_token_list(std::vector<llama_token> *state_token_list, std::vector<llama_token> *input_tokens, llama_token bos_token);
 
