35f1ccf1db68689830cccb5270893e2763ca5133
Distance <egoistgray@gmail.com>
2024-06-25 16:39:35 +0800

    feat: disabled openmp

--- build-cpu.ps1
+++ build-cpu.ps1
@@ -4,6 +4,7 @@ cmake -B build `
    -D CMAKE_CXX_COMPILER=clang-cl `
    -D CMAKE_BUILD_TYPE=Release `
    -D CMAKE_EXPORT_COMPILE_COMMANDS=ON `
-   -D LLAMA_NATIVE=OFF
+   -D LLAMA_NATIVE=OFF `
+   -D LLAMA_OPENMP=OFF
 cmake --build build -j24
 cmake --install build
--- build-cuda.ps1
+++ build-cuda.ps1
@@ -6,6 +6,7 @@ cmake -B build `
    -D CMAKE_BUILD_TYPE=Release `
    -D LLAMA_CUDA=ON `
    -D CMAKE_EXPORT_COMPILE_COMMANDS=ON `
-   -D LLAMA_NATIVE=OFF
+   -D LLAMA_NATIVE=OFF `
+   -D LLAMA_OPENMP=OFF
 cmake --build build -j24
 cmake --install build
